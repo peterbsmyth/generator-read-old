3fda24ab0a07f1b149b613a6c74fecc2ff644b95
Peter B Smith <peterbutlersmith@gmail.com>
2017-02-19 14:54:52 -0500

    initial(demo): does the bare minimum to work

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # generator-read
 A generator of Markdown Templates for read
+
+
+### ISSUES
+- I use `\`${templates}\`` without handling browsers that can't handle templates
---
+++ js/app.js
@@ -0,0 +1,21 @@
+angular.module('read', ['ngFileSaver']);
+
+angular.module('read')
+.controller('ReadCtrl', function($scope, FileSaver, Blob) {
+    $scope.data;
+
+    $scope.fn = {
+      generate: generate
+    };
+
+    function generate(form) {
+      var title = `# ${$scope.data.title} \n`;
+      var subtitle = `## ${$scope.data.subtitle} \n`;
+      var author = `## ${$scope.data.author} \n`;
+      var amazon = `[Amazon](${$scope.data.amazon}) \n`;
+      var data = new Blob([title, subtitle, author, amazon], { type: 'text/plain;charset=utf-8' });
+      FileSaver.saveAs(data, 'text.txt');
+    }
+
+
+});
