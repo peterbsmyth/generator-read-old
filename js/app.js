fb266beca9970e51f7efcf324e71051bfb006480
Peter B Smith <peterbutlersmith@gmail.com>
2017-04-19 08:48:32 -0400

    add: chapter generator

--- js/app.js
+++ js/app.js
@@ -1,14 +1,59 @@
-angular.module('read', ['ngFileSaver']);
+angular.module('read', ['ngFileSaver','ui.router']);
 
 angular.module('read')
-.controller('ReadCtrl', function($scope, FileSaver, Blob) {
-    $scope.data;
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/readme');
+
+  $stateProvider
+    .state('readme', {
+      url: "/readme",
+      templateUrl: "partials/readme.html",
+      controller: 'ReadCtrl'
+    })
+    .state('chapter', {
+      url: "/chapter",
+      templateUrl: "partials/chapter.html",
+      controller: 'ReadCtrl'
+    });
+
+})
+
+angular.module('read')
+.factory('DataFactory', function() {
+  function set(data, section) {
+    this._data[section] = data;
+  }
+
+  function get(section) {
+    return this._data[section] || null;
+  }
+
+  return {
+    _data: {},
+    set: set,
+    get: get
+  }
+});
+
+angular.module('read')
+.controller('ReadCtrl', function($scope, FileSaver, Blob, DataFactory) {
+    $scope.data = {};
+    $scope.data.title = DataFactory.get('title');
+    $scope.data.subtitle = DataFactory.get('subtitle');
+    $scope.data.author = DataFactory.get('author');
+    $scope.data.amazon = DataFactory.get('amazon');
+    $scope.data.bn = DataFactory.get('bn');
 
     $scope.fn = {
-      generate: generate
+      generateReadme: generateReadme,
+      generateChapter: generateChapter,
+      setData: setData,
+      // getData: getData
     };
 
-    function generate(form) {
+
+
+    function generateReadme(form) {
       var title = `# ${$scope.data.title} \n`;
       var subtitle = `## ${$scope.data.subtitle} \n`;
       var author = `## ${$scope.data.author} \n`;
@@ -17,6 +62,23 @@ angular.module('read')
       var data = new Blob([title, subtitle, author, buy, chapters], { type: 'text/plain;charset=utf-8' });
       FileSaver.saveAs(data, 'README.md');
     }
+
+    function generateChapter(form) {
+      var title = `# ${$scope.data.title} \n`;
+      var subtitle = `## ${$scope.data.subtitle} \n`;
+      var author = `## ${$scope.data.author} \n`;
+      var buy = `[Amazon](${$scope.data.amazon}) | [B&N](${$scope.data.bn}) \n\n`;
+      var chapter = `#### ${$scope.data.chapterTitle} \n`;
+      var data = new Blob([title, subtitle, author, buy, chapter], { type: 'text/plain;charset=utf-8' });
+      FileSaver.saveAs(data, `chapter_${$scope.data.chapterNum}.md`);
+    }
+
+    function setData(section) {
+      console.log('action');
+      DataFactory.set($scope.data[section], section);
+    }
+
+    // function getData()
 
 
 });
